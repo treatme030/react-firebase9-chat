24b14ce77ea39cdaee7c1689b6f04006f5d452c7
treatme030 <treatme8192@gmail.com>
2022-07-31 23:57:16 +0900

    add register, login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
         <Route exact path='/' component={Home} />
       </Switch>
     </BrowserRouter>
